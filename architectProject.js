b7b5815611c84c6cd09d06588a99d49ae5c13b2b
RadostinaDraganska <145573440+RadostinaDraganska@users.noreply.github.com>
2023-09-20 16:56:05 +0300

    Delete architectProject.js

--- architectProject.js
+++
@@ -1,8 +0,0 @@
-function architectProject(input) {
-    let name=input[0];
-    let projectsNum=Number(input[1]);
-    let hours=projectsNum * 3;
-    console.log(`The architect ${name} will need ${hours} hours to complete ${projectsNum} project/s.`);
-
-}
-architectProject(["Sanya ", "9 "]);
